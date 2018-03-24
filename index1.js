ec1adf09013103353c7cd5df736d4669107873b4
saswati1994 <saswatidashinfo@gmail.com>
2018-03-24 04:20:54 -0700

    first commit

---
+++ index1.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var bodyParser = require('body-parser')
+
+var app = express()
+app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.json());
+app.get('/', function(request, response) {
+response.send('Hello World!')
+})
+app.post('/webhook',function(request,response){
+//console.log(JSON.stringify(request.body));
+console.log("WEBHOOK TRIGRED")
+})
+app.listen(app.get('port'), function() {
+console.log("Node app is running at localhost:" + app.get('port'))
+})
