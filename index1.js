2e94d796078ddc1998332c23f4af54812fc1ce1f
saswati1994 <34511388+saswati1994@users.noreply.github.com>
2018-03-29 11:15:41 -0700

    Update index1.js

--- index1.js
+++ index1.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
 response.send('Hello World!')
 })
 app.post('/webhook',function(request,response){
-where jsondata=JSON.parse(request.body);
+var jsondata=JSON.parse(request.body);
 console.log(jsondata); 
 //console.log(JSON.stringify(request.body));
 console.log("WEBHOOK TRIGRED")
