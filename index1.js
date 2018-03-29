07a878979505ff83e4404ba171b3ece09ba11f96
saswati1994 <34511388+saswati1994@users.noreply.github.com>
2018-03-29 11:41:35 -0700

    Update index1.js

--- index1.js
+++ index1.js
@@ -4,15 +4,15 @@ var bodyParser = require('body-parser')
 
 var app = express()
 app.set('port', (process.env.PORT || 5000))
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.get('/', function(request, response) {
 response.send('Hello World!')
 })
 app.post('/webhook',function(request,response){
-   console.log(request)
-   console.log(request.body)
-var jsondata=JSON.parse(request.body);
-console.log(jsondata); 
+  // console.log(request)
+  // console.log(request.body)
+//var jsondata=JSON.parse(request.body);
+console.log(request.body); 
 //console.log(JSON.stringify(request.body));
 console.log("WEBHOOK TRIGRED")
 
