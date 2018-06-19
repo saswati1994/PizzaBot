1871664fbd013f130acc4ac5e04551dade1c9244
saswati1994 <34511388+saswati1994@users.noreply.github.com>
2018-06-19 19:29:09 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -74,6 +74,12 @@ function cabrequesthandler(request,response){
 }
 function locationhandler(request,response){
     console.log(JSON.stringify(request.body));
+  var data= JSON.parse(request.body);
+  var lat = req.body.originalRequest.data.postback.data.lat;
+
+var long = req.body.originalRequest.data.postback.data.long;
+  console.log(lat);
+  console.log(long);
   }
 
 //function to generate facebook response format 
