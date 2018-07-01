088a2c60e668edee6338849b06212ab0519ee4ce
Abhishek Dash <36022@hexaware.com>
2018-07-01 15:17:11 +0530

    location response

--- index1.js
+++ index1.js
@@ -137,7 +137,7 @@ function NearbyPalceSearch(lat,long,callback){
     url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
     qs: 
     { location: lat+","+long,
-      radius: '500',
+      radius: '1000',
       type: 'atm',
       key: 'AIzaSyAvsCXxI6RRtBWzQB9nmdnNbxsksAwLjEA' },
     headers: 
