c7b2d148c51b48ea88ea91690672fc3f957df5a0
saswati1994 <34511388+saswati1994@users.noreply.github.com>
2018-07-30 01:25:41 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -89,6 +89,7 @@ function locationhandler(request,response){
   console.log(lat);
   console.log(long);
   var targets=[];
+  console.log(request.body.queryResult.outputContexts[0].parameters)
   var search = request.body.queryResult.outputContexts[0].parameters.poi;
   console.log("SEARCH PARAMETER",search);
    NearbyPalceSearch(lat,long,search,(err,data)=>{
